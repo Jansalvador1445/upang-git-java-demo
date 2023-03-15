f749161852846f28fa0ecc5722e408ab33298853
Jan Salvador Sebastian <sebastian@mclinica.com>
2023-03-15 10:18:46 +0800

    Added codes

--- sample.java
+++ sample.java
@@ -1,11 +1,15 @@
 class Sample {
     public static void main(String [] args) {
-
-
-
-
-
-
-        
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
+        System.out.println("Reynard Visperas Guapo");
     }
 }
